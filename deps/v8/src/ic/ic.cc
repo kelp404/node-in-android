64584ed009589d34270b26fcafee092afdb6f6bb
Kelp <kelp@phate.org>
2018-09-11 15:17:15 +0800

    Replaced `std::to_string()`.

--- deps/v8/src/ic/ic.cc
+++ deps/v8/src/ic/ic.cc
@@ -161,7 +161,9 @@ void IC::TraceIC(const char* type, Handle<Object> name, State old_state,
   if (map != nullptr) {
     ic_info.is_dictionary_map = map->is_dictionary_map();
     ic_info.number_of_own_descriptors = map->NumberOfOwnDescriptors();
-    ic_info.instance_type = std::to_string(map->instance_type());
+    std::ostringstream sstr;
+    sstr << map->instance_type();
+    ic_info.instance_type = sstr.str();
   }
   // TODO(lpy) Add name as key field in ICStats.
   ICStats::instance()->End();
