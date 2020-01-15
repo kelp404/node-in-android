b42755d2a9aa3dc7d290973443c78f141d4ad6c9
Kelp <kelp@phate.org>
2020-01-15 16:10:50 +0800

    Upgraded Android API level to 22.

--- android-build-tools/build.sh
+++ android-build-tools/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-build-nodejs-for-android arm64 --api 21 -j3 -o ../node-android-21-arm64
-build-nodejs-for-android x64 --api 21 -j3 -o ../node-android-21-x64
+build-nodejs-for-android arm64 --api 22 -j3 -o ../node-android-22-arm64
+build-nodejs-for-android x64 --api 22 -j3 -o ../node-android-22-x64
